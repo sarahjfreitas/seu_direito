f04eff40636ae368acbf4142ad637eca36b43aa4
sarahjfreitas <sarahjfreitas@gmail.com>
2019-03-19 22:54:13 +0000

    layout inicial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+//= require jquery.min
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
+// require turbolinks
+//= require bootstrap.min
+//= require bootstrap-checkbox-radio
+//= require chartist.min
+//= require bootstrap-notify
+//= require paper-dashboard
+//= require demo
+// <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+//= require_tree
