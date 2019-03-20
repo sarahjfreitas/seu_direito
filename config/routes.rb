edc29091c2cb2feace25a54f22c7ec62e2241da5
sarahjfreitas <sarahjfreitas@gmail.com>
2019-03-20 01:38:08 +0000

    devise config e criacao do home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   resources :lawyers
   resources :companies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
