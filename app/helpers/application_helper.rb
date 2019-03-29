1e864934bc3efe9373f9dc25a2cda6d45cff0389
Sarah Julia <sarahjfreitas@gmail.com>
2019-03-29 01:13:32 -0300

    tela de cadastro de usuario

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
     ('<div class="pull-right"><button type="submit" class="btn btn-primary"><span class="fa fa-sign-in"></span> '+t('messages.login') +' </button></div>').html_safe
   end
 
+  def sign_in
+    ('<div class="pull-right"><button type="submit" class="btn btn-primary"><span class="fa fa-sign-in"></span> '+t('messages.sign_in') +' </button></div>').html_safe
+  end
+
   def show_error_notification(mensagem)
   end
 end
